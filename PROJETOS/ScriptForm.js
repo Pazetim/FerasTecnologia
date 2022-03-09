ed8c66d578afb4a835730b72517e8932db117522
Pazetim <99148959+Pazetim@users.noreply.github.com>
2022-03-08 21:41:21 -0300

    PageForm e Att LandPage

---
+++ PROJETOS/ScriptForm.js
@@ -0,0 +1,10 @@
+let form = document.getElementById("register-form")
+let form = document.getElementById("btn-submit")
+
+SubmitEvent.addEventListener('click', function(e) {
+
+    e.preventDefault();
+
+    console.log('funcionou')
+
+});
